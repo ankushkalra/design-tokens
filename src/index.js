b979fd686e2acfee0bf7f23c36229f96ddf24430
Ankush Kalra <ankushkalra2@gmail.com>
2026-08-28 20:49:38 +0530

    added theme based variables

--- src/index.js
+++ src/index.js
@@ -1,26 +1,53 @@
-import { StyleDictionary } from "style-dictionary-utils";
-import { outputReferencesTransformed } from "style-dictionary/utils";
+import fs from "fs";
+import StyleDictionary from "style-dictionary";
 
-const myStyleDictionary = new StyleDictionary();
-
-const config = {
-  source: ["tokens/**/*.tokens.json"],
+const darkSD = new StyleDictionary({
+  source: ["tokens/base/*.tokens.json", "tokens/dark/*.tokens.json"],
   platforms: {
     css: {
       buildPath: "dist/css/",
       transformGroup: "css",
       files: [
         {
+          destination: "_dark.css",
           format: "css/variables",
-          destination: "variables.css",
           options: {
-            outputReferences: false,
+            selector: ":root",
           },
         },
       ],
     },
   },
-};
+  log: {
+    verbosity: "verbose",
+  },
+});
 
-const sd = await myStyleDictionary.extend(config);
-sd.buildAllPlatforms();
+const lightSD = new StyleDictionary({
+  source: ["tokens/base/*.tokens.json", "tokens/light/*.tokens.json"],
+  platforms: {
+    css: {
+      buildPath: "dist/css/",
+      transformGroup: "css",
+      files: [
+        {
+          destination: "_light.css",
+          format: "css/variables",
+          options: {
+            selector: '[data-theme="light"]',
+          },
+        },
+      ],
+    },
+  },
+  log: {
+    verbosity: "verbose",
+  },
+});
+
+await darkSD.buildAllPlatforms();
+await lightSD.buildAllPlatforms();
+
+const dark = fs.readFileSync("dist/css/_dark.css", "utf8");
+const light = fs.readFileSync("dist/css/_light.css", "utf8");
+fs.writeFileSync("dist/css/variables.css", dark + "\n" + light);
